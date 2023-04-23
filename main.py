e8df92c47c51b30dd271d2e0c996f68442d1ba7b
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 20:51:17 +0000

    update

--- main.py
+++ main.py
@@ -2,25 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# define two different medical codexes
-icd_codes = [
-    {"code": "A00", "description": "Cholera"},
-    {"code": "A01", "description": "Typhoid and paratyphoid fevers"},
-    {"code": "A02", "description": "Other salmonella infections"},
-]
+@app.get("/icd10")
+def get_icd10():
+    return {"icd10": ["A00", "A01", "A02", ... ]}
 
-cpt_codes = [
-    {"code": "10021", "description": "Fine needle aspiration; without imaging guidance"},
-    {"code": "10022", "description": "Fine needle aspiration; with imaging guidance"},
-    {"code": "10030", "description": "Guidance for insertion of needle into muscle for electromyography"},
-]
+@app.get("/cpt")
+def get_cpt():
+    return {"cpt": ["10021", "10022", "10030", ... ]}
 
-@app.get("/icd_codes")
-async def get_icd_codes():
-    return icd_codes
-
-@app.get("/cpt_codes")
-async def get_cpt_codes():
-    return cpt_codes
 
 
