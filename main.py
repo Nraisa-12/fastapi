dbd5e4f332d485942ad7e73fe760643b40e4b508
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 22:12:18 +0000

    update

--- main.py
+++ main.py
@@ -2,24 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/icd")
+async def get_icd_codes():
+    icd1 = {
+        "code": "A01.1",
+        "description": "Typhoid fever"
+    }
+    icd2 = {
+        "code": "B25.9",
+        "description": "Cytomegaloviral disease, unspecified"
+    }
+    return {"icd1": icd1, "icd2": icd2}
 
-icd10 = {
-    "A00": "Cholera",
-    "A01": "Typhoid fever",
-    "A02": "Salmonella infections",
-    # ...
-}
-
-cpt = {
-    "10060": "Incision and drainage of abscess",
-    "99213": "Office or other outpatient visit for the evaluation and management of an established patient",
-    "99214": "Office or other outpatient visit for the evaluation and management of an established patient",
-    # ...
-}
-
-@app.get("/medical_codexes")
-async def get_medical_codexes():
-    return {"icd10": icd10, "cpt": cpt}
 
 
 
