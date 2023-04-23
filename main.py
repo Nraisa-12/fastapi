0e75e71f9ccef180e01991d121778ed5d78674b1
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 21:57:46 +0000

    update

--- main.py
+++ main.py
@@ -2,30 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-medical_codexes = {
-    "icd-10": {
-        "description": "International Classification of Diseases, 10th Revision",
-        "url": "https://www.who.int/classifications/icd/icdonlineversions/en/"
-    },
-    "cpt": {
-        "description": "Current Procedural Terminology",
-        "url": "https://www.ama-assn.org/practice-management/cpt"
-    }
-}
+medical_codex1 = {"code": "ABC123", "name": "Medical Codex 1"}
+medical_codex2 = {"code": "DEF456", "name": "Medical Codex 2"}
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the medical codex API."}
+    return {"Medical Codex API"}
 
-@app.get("/codexes/")
-async def get_codexes():
-    return medical_codexes
+@app.get("/codex1")
+async def get_codex1():
+    return medical_codex1
 
-@app.get("/codexes/{codex_name}")
-async def get_codex(codex_name: str):
-    if codex_name not in medical_codexes:
-        return {"error": "Invalid medical codex."}
-    return medical_codexes[codex_name]
+@app.get("/codex2")
+async def get_codex2():
+    return medical_codex2
+
 
 
 
