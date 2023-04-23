d92558a47820fa9e00fe4a85bb556d1b11730449
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 21:52:23 +0000

    update

--- main.py
+++ main.py
@@ -2,32 +2,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-codexes = {
-    "icd10": [
-        {"code": "A00", "description": "Cholera"},
-        {"code": "B10", "description": "Viral hepatitis"},
-        {"code": "C15", "description": "Malignant neoplasm of oesophagus"},
-    ],
-    "cpt": [
-        {"code": "10021", "description": "Fine needle aspiration; without imaging guidance"},
-        {"code": "99213", "description": "Office or other outpatient visit for the evaluation and management of an established patient, which requires at least 2 of these 3 key components: a problem focused history; a problem focused examination; straightforward medical decision making."},
-        {"code": "96372", "description": "Therapeutic, prophylactic, or diagnostic injection (specify substance or drug); subcutaneous or intramuscular"},
-    ],
+medical_codexes = {
+    "icd-10": {
+        "description": "International Classification of Diseases, 10th Revision",
+        "url": "https://www.who.int/classifications/icd/icdonlineversions/en/"
+    },
+    "cpt": {
+        "description": "Current Procedural Terminology",
+        "url": "https://www.ama-assn.org/practice-management/cpt"
+    }
 }
 
 @app.get("/")
 async def root():
-    return {"message": "Medical codex API."}
+    return {"message": "Welcome to the medical codex API."}
 
-@app.get("/codexes")
+@app.get("/codexes/")
 async def get_codexes():
-    return codexes
+    return medical_codexes
 
-@app.get("/codexes/{codex}")
-async def get_codex(codex: str):
-    if codex not in codexes:
-        return {"error": f"Invalid codex '{codex}'. Allowed codexes are {', '.join(codexes.keys())}."}
-    return codexes[codex]
+@app.get("/codexes/{codex_name}")
+async def get_codex(codex_name: str):
+    if codex_name not in medical_codexes:
+        return {"error": "Invalid medical codex."}
+    return medical_codexes[codex_name]
+
 
 
 
