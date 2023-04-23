dc08ae3ae029cf042c8921307cc6ea1c5b3310fd
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 21:47:43 +0000

    update

--- main.py
+++ main.py
@@ -2,12 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-med_codex1 = ["code1", "code2", "code3"]
-med_codex2 = ["codeA", "codeB", "codeC"]
+codexes = {
+    "icd10": [
+        {"code": "A00", "description": "Cholera"},
+        {"code": "B10", "description": "Viral hepatitis"},
+        {"code": "C15", "description": "Malignant neoplasm of oesophagus"},
+    ],
+    "cpt": [
+        {"code": "10021", "description": "Fine needle aspiration; without imaging guidance"},
+        {"code": "99213", "description": "Office or other outpatient visit for the evaluation and management of an established patient, which requires at least 2 of these 3 key components: a problem focused history; a problem focused examination; straightforward medical decision making."},
+        {"code": "96372", "description": "Therapeutic, prophylactic, or diagnostic injection (specify substance or drug); subcutaneous or intramuscular"},
+    ],
+}
 
-@app.get("/med_codexes")
-async def get_med_codexes():
-    return {"med_codex1": med_codex1, "med_codex2": med_codex2}
+@app.get("/")
+async def root():
+    return {"message": "Medical codex API."}
+
+@app.get("/codexes")
+async def get_codexes():
+    return codexes
+
+@app.get("/codexes/{codex}")
+async def get_codex(codex: str):
+    if codex not in codexes:
+        return {"error": f"Invalid codex '{codex}'. Allowed codexes are {', '.join(codexes.keys())}."}
+    return codexes[codex]
+
 
 
 
