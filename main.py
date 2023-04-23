e4c9bab779d30a7505e7e2f4014e2a846b80418a
noshinrasa <noshin.raisa@stonybrook.edu>
2023-04-23 19:26:19 +0000

    update

---
+++ main.py
@@ -0,0 +1,8 @@
+from time import time
+from fastapi import FastAPI, __version__
+
+app = FastAPI() # This is what will be refrenced in config
+
+@app.get('/')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
